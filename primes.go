7ba72ba0e35a52bc98f268a2fb44f1137cbd45c4
gummi <thatstealthyowl@gmail.com>
2024-11-17 20:57:41 -0600

    golang and edit script

---
+++ primes.go
@@ -0,0 +1,28 @@
+package primes
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	num := 2
+	for {
+		if isPrime(num) {
+			fmt.Println(num)
+		}
+		num++
+	}
+}
